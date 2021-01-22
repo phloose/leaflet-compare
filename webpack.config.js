cd43b28bfbeed590a9d43d3cee3485983a1bc843
Philipp Loose <phloose@mailbox.org>
2021-01-22 20:48:12 +0100

    Use webpack-dev-server instead of budo
    Since the project uses webpack stay with tools that are offered by
    webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,9 @@ module.exports = [
         module: {
             rules: rules,
         },
+        devServer: {
+            index: "./index.html",
+            publicPath: "./src/",
+        },
     },
 ];
