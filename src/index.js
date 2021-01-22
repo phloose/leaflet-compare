f5d76c102958b077a76e3f0777699af7154e9c3e
Philipp Loose <phloose@mailbox.org>
2021-01-22 23:48:59 +0100

    Format module according to eslint rules

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-var L = require("leaflet");
+const L = require("leaflet");
 require("./layout.css");
 require("./range.css");
 
-var mapWasDragEnabled;
-var mapWasTapEnabled;
+let mapWasDragEnabled;
+let mapWasTapEnabled;
 
 // Leaflet v0.7 backwards compatibility
 function on(el, types, fn, context) {
-    types.split(" ").forEach(function (type) {
+    types.split(" ").forEach(type => {
         L.DomEvent.on(el, type, fn, context);
     });
 }
 
 // Leaflet v0.7 backwards compatibility
 function off(el, types, fn, context) {
-    types.split(" ").forEach(function (type) {
+    types.split(" ").forEach(type => {
         L.DomEvent.off(el, type, fn, context);
     });
 }
@@ -45,9 +45,7 @@ function asArray(arg) {
     return arg === "undefined" ? [] : Array.isArray(arg) ? arg : [arg];
 }
 
-function noop() {
-    return;
-}
+function noop() {}
 
 L.Control.SplitMap = L.Control.extend({
     options: {
@@ -55,18 +53,17 @@ L.Control.SplitMap = L.Control.extend({
         padding: 0,
     },
 
-    initialize: function (leftLayers, rightLayers, options) {
+    initialize(leftLayers, rightLayers, options) {
         this._leftLayers = asArray(leftLayers);
         this._rightLayers = asArray(rightLayers);
         this._updateClip();
         L.setOptions(this, options);
     },
 
-    getPosition: function () {
-        var rangeValue = this._range.value;
-        var offset =
-            (0.5 - rangeValue) *
-            (2 * this.options.padding + this.options.thumbSize);
+    getPosition() {
+        const rangeValue = this._range.value;
+        const offset = (0.5 - rangeValue)
+            * (2 * this.options.padding + this.options.thumbSize);
         return this._map.getSize().x * rangeValue + offset;
     },
 
@@ -74,41 +71,40 @@ L.Control.SplitMap = L.Control.extend({
 
     includes: L.Mixin.Events,
 
-    addTo: function (map) {
+    addTo(map) {
         this.remove();
         this._map = map;
-        var container = (this._container = L.DomUtil.create(
+        const container = (this._container = L.DomUtil.create(
             "div",
             "leaflet-sbs",
-            map._controlContainer
+            map._controlContainer,
         ));
         this._divider = L.DomUtil.create(
             "div",
             "leaflet-sbs-divider",
-            container
+            container,
         );
-        var range = (this._range = L.DomUtil.create(
+        const range = (this._range = L.DomUtil.create(
             "input",
             "leaflet-sbs-range",
-            container
+            container,
         ));
         range.type = "range";
         range.min = 0;
         range.max = 1;
         range.step = "any";
         range.value = 0.5;
-        range.style.paddingLeft = range.style.paddingRight =
-            this.options.padding + "px";
+        range.style.paddingLeft = range.style.paddingRight = `${this.options.padding}px`;
         this._addEvents();
         this._updateClip();
         return this;
     },
 
-    remove: function () {
+    remove() {
         if (!this._map) {
             return this;
         }
-        this._leftLayers.forEach((left_layer) => {
+        this._leftLayers.forEach(left_layer => {
             if (left_layer.getContainer) {
                 left_layer.getContainer().style.clip = "";
             } else {
@@ -116,7 +112,7 @@ L.Control.SplitMap = L.Control.extend({
             }
         });
 
-        this._rightLayers.forEach((right_layer) => {
+        this._rightLayers.forEach(right_layer => {
             if (right_layer.getContainer) {
                 right_layer.getContainer().style.clip = "";
             } else {
@@ -129,21 +125,21 @@ L.Control.SplitMap = L.Control.extend({
         return this;
     },
 
-    _updateClip: function () {
+    _updateClip() {
         if (!this._map) {
             return this;
         }
-        var map = this._map;
-        var nw = map.containerPointToLayerPoint([0, 0]);
-        var se = map.containerPointToLayerPoint(map.getSize());
-        var clipX = nw.x + this.getPosition();
-        var dividerX = this.getPosition();
-        this._divider.style.left = dividerX + "px";
+        const map = this._map;
+        const nw = map.containerPointToLayerPoint([0, 0]);
+        const se = map.containerPointToLayerPoint(map.getSize());
+        const clipX = nw.x + this.getPosition();
+        const dividerX = this.getPosition();
+        this._divider.style.left = `${dividerX}px`;
         this.fire("dividermove", { x: dividerX });
-        var clipLeft = "rect(" + [nw.y, clipX, se.y, nw.x].join("px,") + "px)";
-        var clipRight = "rect(" + [nw.y, se.x, se.y, clipX].join("px,") + "px)";
+        const clipLeft = `rect(${[nw.y, clipX, se.y, nw.x].join("px,")}px)`;
+        const clipRight = `rect(${[nw.y, se.x, se.y, clipX].join("px,")}px)`;
 
-        this._leftLayers.forEach((left_layer) => {
+        this._leftLayers.forEach(left_layer => {
             if (left_layer.getContainer) {
                 left_layer.getContainer().style.clip = clipLeft;
             } else {
@@ -151,7 +147,7 @@ L.Control.SplitMap = L.Control.extend({
             }
         });
 
-        this._rightLayers.forEach((right_layer) => {
+        this._rightLayers.forEach(right_layer => {
             if (right_layer.getContainer) {
                 right_layer.getContainer().style.clip = clipRight;
             } else {
@@ -160,9 +156,9 @@ L.Control.SplitMap = L.Control.extend({
         });
     },
 
-    _addEvents: function () {
-        var range = this._range;
-        var map = this._map;
+    _addEvents() {
+        const range = this._range;
+        const map = this._map;
         if (!map || !range) return;
         map.on("move", this._updateClip, this);
         map.on("layeradd layerremove", this._updateLayers, this);
@@ -171,32 +167,32 @@ L.Control.SplitMap = L.Control.extend({
             range,
             L.Browser.touch ? "touchstart" : "mousedown",
             cancelMapDrag,
-            this
+            this,
         );
         on(
             range,
             L.Browser.touch ? "touchend" : "mouseup",
             uncancelMapDrag,
-            this
+            this,
         );
     },
 
-    _removeEvents: function () {
-        var range = this._range;
-        var map = this._map;
+    _removeEvents() {
+        const range = this._range;
+        const map = this._map;
         if (range) {
             off(range, getRangeEvent(range), this._updateClip, this);
             off(
                 range,
                 L.Browser.touch ? "touchstart" : "mousedown",
                 cancelMapDrag,
-                this
+                this,
             );
             off(
                 range,
                 L.Browser.touch ? "touchend" : "mouseup",
                 uncancelMapDrag,
-                this
+                this,
             );
         }
         if (map) {
