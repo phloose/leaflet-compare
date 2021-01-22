40f42cfac8c938faa61eaa753635b84e15ff7ae8
Philipp Loose <phloose@mailbox.org>
2021-01-22 20:37:39 +0100

    Apply more readable formatting to main module and configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
-var path = require('path');
-var version = require('./package.json').version;
+var path = require("path");
+var version = require("./package.json").version;
 
 var rules = [
     {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
     },
     {
         test: /\.(jpg|png|gif|svg)$/,
         use: [
             {
-                loader: 'file-loader',
+                loader: "file-loader",
                 options: {
-                    publicPath: "/dist/"
-                }
-            }
-        ]
+                    publicPath: "/dist/",
+                },
+            },
+        ],
     },
 ];
 
 module.exports = [
-    {// embeddable jupyter-leaflet bundle
-        entry: './src/index.js',
+    {
+        // embeddable jupyter-leaflet bundle
+        entry: "./src/index.js",
         output: {
-            filename: 'leaflet-splitmap.js',
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: "/dist/"
+            filename: "leaflet-splitmap.js",
+            path: path.resolve(__dirname, "dist"),
+            publicPath: "/dist/",
         },
-        devtool: 'source-map',
+        devtool: "source-map",
         module: {
-            rules: rules
-        }
-    }
+            rules: rules,
+        },
+    },
 ];
