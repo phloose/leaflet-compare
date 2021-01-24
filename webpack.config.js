53a292cc27d89e012ef3bdb79616d41991d1a197
Philipp Loose <phloose@mailbox.org>
2021-01-24 21:53:43 +0100

    Rearrange webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,43 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { version } = require("./package.json");
 
-const rules = [
-    {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+module.exports = {
+    // embeddable jupyter-leaflet bundle
+    entry: "./src/index.js",
+    output: {
+        filename: "leaflet-splitmap.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist/",
     },
-    {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: [
+    devtool: "source-map",
+    module: {
+        rules: [
             {
-                loader: "url-loader",
-                options: {
-                    limit: 8000,
-                    name: "images/[hash]-[name].[ext]",
-                },
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8000,
+                            name: "images/[hash]-[name].[ext]",
+                        },
+                    },
+                ],
             },
         ],
     },
-];
-
-module.exports = [
-    {
-        // embeddable jupyter-leaflet bundle
-        entry: "./src/index.js",
-        output: {
-            filename: "leaflet-splitmap.js",
-            path: path.resolve(__dirname, "dist"),
-            publicPath: "/dist/",
-        },
-        devtool: "source-map",
-        module: {
-            rules,
-        },
-        externals: {
-            leaflet: "L",
-        },
-        devServer: {
-            index: "./index.html",
-            publicPath: "./src/",
-        },
-        plugins: [new MiniCssExtractPlugin({
-            filename: "leaflet-splitmap.css",
-        })],
+    externals: {
+        leaflet: "L",
     },
-];
+    devServer: {
+        index: "./index.html",
+        publicPath: "./src/",
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: "leaflet-splitmap.css",
+    })],
+};
